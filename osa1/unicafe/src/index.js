b1e4979ea385bd8bab7b2d16bbf9829f7853dfe2
hilma <hilma.holappa@helsinki.fi>
2019-01-21 21:34:34 +0200

    Tehtävä 1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Statistics = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+          <div>
+            Ei yhtään annettua palautetta
+          </div>
+        )
+      }
     return (
         <div>
         <h1>Statistiikka:</h1>
@@ -18,25 +25,37 @@ const Statistics = (props) => {
   }
 
 const App = (props) => {
-    const [ good, setGood] = useState(0)
+    const [ good, setGood ] = useState(0)
     const [ neutral, setNeutral ] = useState(0)
-    const [ bad, setBad] = useState(0)
+    const [ bad, setBad ] = useState(0)
+    const [allClicks, setAll] = useState([])
     const header = 'Anna palautetta:'
     const stats = {
         part1: 'Hyvä',
         part2: 'Neutraali',
         part3: 'Huono'
     }
-    
-    
-    
+    const goodClick = () => {
+        setAll(allClicks.concat('C'))
+        setGood(good + 1)
+      }
+      const neutralClick = () => {
+        setAll(allClicks.concat('C'))
+        setNeutral(neutral + 1)
+      }
+      const badClick = () => {
+        setAll(allClicks.concat('C'))
+        setBad(bad + 1)
+      }
     return (
       <div>
         <h1>{header}</h1>
-        <button onClick={() => setGood(good + 1)}>Hyvä</button>
-        <button onClick={() => setNeutral(neutral + 1)}>Neutraali</button>
-        <button onClick={() => setBad(bad + 1)}>Huono</button>
-        <Statistics stats={stats} good={good} neutral={neutral} bad={bad}/>
+        <button onClick={goodClick}>Hyvä</button>
+        <button onClick={neutralClick}>Neutraali</button>
+        <button onClick={badClick}>Huono</button>
+        <Statistics stats={stats} 
+        good={good} neutral={neutral} bad={bad}
+        allClicks={allClicks}/>
       </div>
     )
 
