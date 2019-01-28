a94a02590842d09da8575323cbbd3f06dceb82ac
hilma <hilma.holappa@helsinki.fi>
2019-01-28 15:57:05 +0200

    Tehtävä 1.10 oikein

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,75 +1,82 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-const Statistics = (props) => {
-    if (props.allClicks.length === 0) {
-        return (
-          <div>
-            Ei yhtään annettua palautetta
-          </div>
-        )
-      }
-    return (
-        <div>
-        <h1>Statistiikka:</h1>
-        <p>{props.stats.part1} {props.good}</p>
-        <p>{props.stats.part2} {props.neutral}</p>
-        <p>{props.stats.part3} {props.bad}</p>
-        <p>Yhteensä: {props.good+props.neutral+props.bad}</p>
-        <p>Keskiarvo: {(props.good-props.bad)/(props.good+props.neutral+props.bad)}</p>
-        <p>Positiivisia: {props.good/(props.good+props.neutral+props.bad)*100} %</p>
-        </div>
-    )
-    
-    
+const Statistics = props => {
+  if (props.allClicks.length === 0) {
+    return <div>Ei yhtään annettua palautetta</div>;
   }
+  return (
+    <div>
+      <h1>Statistiikka:</h1>
+      <StatisticItem name="Hyvä: " value={props.good} />
+      <StatisticItem name="Neutraali: " value={props.neutral} />
+      <StatisticItem name="Huono: " value={props.bad} />
+      <StatisticItem
+        name="Yhteensä: "
+        value={props.good + props.neutral + props.bad}
+      />
+      <StatisticItem
+        name="Keskiarvo: "
+        value={
+          (props.good - props.bad) / (props.good + props.neutral + props.bad)
+        }
+      />
+      <StatisticItem
+        name="Positiivisia: "
+        value={
+          (props.good / (props.good + props.neutral + props.bad)) * 100 + "%"
+        }
+      />
+    </div>
+  );
+};
 
-const Button = (props) => (
-    <button onClick={props.hClick}>
-        {props.text}
-    </button>
-)
-            
+const StatisticItem = props => {
+  return (
+    <div>
+      <p>
+        {props.name}
+        {props.value}
+      </p>
+    </div>
+  );
+};
 
-const App = (props) => {
-    const [ good, setGood ] = useState(0)
-    const [ neutral, setNeutral ] = useState(0)
-    const [ bad, setBad ] = useState(0)
-    const [allClicks, setAll] = useState([])
-    const header = 'Anna palautetta:'
-    const stats = {
-        part1: 'Hyvä',
-        part2: 'Neutraali',
-        part3: 'Huono'
-    }
-    const goodClick = () => {
-        setAll(allClicks.concat('C'))
-        setGood(good + 1)
-      }
-      const neutralClick = () => {
-        setAll(allClicks.concat('C'))
-        setNeutral(neutral + 1)
-      }
-      const badClick = () => {
-        setAll(allClicks.concat('C'))
-        setBad(bad + 1)
-      }
-    return (
-      <div>
-        <h1>{header}</h1>
-        <Button hClick={goodClick} text='Hyvä'/>
-        <Button hClick={neutralClick} text='Neutraali'/>
-        <Button hClick={badClick} text='Huono'/>
-        <Statistics stats={stats} 
-        good={good} neutral={neutral} bad={bad}
-        allClicks={allClicks}/>
-      </div>
-    )
+const Button = props => <button onClick={props.hClick}>{props.text}</button>;
 
-  }
-  let counter = 1
+const App = props => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const header = "Anna palautetta:";
+  const goodClick = () => {
+    setAll(allClicks.concat("C"));
+    setGood(good + 1);
+  };
+  const neutralClick = () => {
+    setAll(allClicks.concat("C"));
+    setNeutral(neutral + 1);
+  };
+  const badClick = () => {
+    setAll(allClicks.concat("C"));
+    setBad(bad + 1);
+  };
+  return (
+    <div>
+      <h1>{header}</h1>
+      <Button hClick={goodClick} text="Hyvä" />
+      <Button hClick={neutralClick} text="Neutraali" />
+      <Button hClick={badClick} text="Huono" />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        allClicks={allClicks}
+      />
+    </div>
+  );
+};
+let counter = 1;
 
-  ReactDOM.render(
-    <App counter={counter} />, 
-    document.getElementById('root')
-  )
+ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
